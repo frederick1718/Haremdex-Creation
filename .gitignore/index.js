cb65fc93b75f5db360775b607582c0ab7ae08842
frederick1718 <39063322+frederick1718@users.noreply.github.com>
2019-02-09 18:00:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,19 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoire: [], xp: []}).write()
 
 var prefix = ("Hc! ")
 
 bot.on('ready', function() {
-    bot.user.setGame("Command: *help");
-    console.log("Connectedç");
+    bot.user.setGame("faire une grande maintenance ...");
+    console.log("Le bot est connecté !");
 });
 
 bot.login(process.env.TOKEN);
@@ -19,4 +27,42 @@ bot.on('message', message => {
         message.reply("Bien le bonjour. :grin:");
         console.log("Commande Salut effectué");
     }
+
+    if (message.content === prefix + "embed") {
+        var embed = new Discord.RichEmbed()
+            .setTitle("EMBED")
+            .setDescription("Ceci est un embed")
+            .addField(".help","Page d'aide", true)
+            .addField("Embed01","Embed 01 ! :grin: Suivez les tuto de [PZH CODAGE](https://www.youtube.com/c/pzhcodage)", true)
+            .setColor("0x58FAF4")
+            .setFooter("Bon moment parmis nous ! ;)")
+        message.channel.sendEmbed(embed);  
+    }
 });
+
+bot.on('message', message => {
+
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()) {
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+
+        db.get("xp").fill({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] <= 1}).write();
+
+    if (message.content === prefix + "xp") {
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Stat des XP de ${message.author.username}`)
+            .setColor("#58FAF4")
+            .setDescription("Affichge des XP")
+            .addField("XP :", `${xpfinal[1]}`)
+            .setFooter("Enjoy :p")
+        message.channel.send({embed: xp_embed});  
+}}});
