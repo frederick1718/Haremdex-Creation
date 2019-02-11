b861846b0a384db0d9e3b1daa2fb55dd98fa08b0
frederick1718 <39063322+frederick1718@users.noreply.github.com>
2019-02-11 20:23:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,12 +2,9 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const client = new Discord.Client();
 
 const adapter = new FileSync('database.json');
 const db = low(adapter);
-
-db.defaults({ histoire: [], xp: []}).write()
 
 var prefix = ("Hc! ")
 
@@ -16,8 +13,7 @@ bot.on('ready', function() {
     console.log("Le bot est connecté !");
 });
 
-//bot . login ( processus . env . JETON );
-bot . login ( NDc2MDI2NzQzNDc2NzE1NTMx.D0JN4A.X4s8nX-sdsR5NC1xq01Mw--StLo ) plus grand centre commercial
+bot . login ( processus . env . JETON );
 
 bot.on('message', message => {
     if (message.content === prefix + "help") {
@@ -40,30 +36,3 @@ bot.on('message', message => {
         message.channel.sendEmbed(embed);  
     }
 });
-
-bot.on('message', message => {
-
-    var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()) {
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb)
-        console.log(`Nombre d'xp: ${userxp[1]}`)
-
-        db.get("xp").fill({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] <= 1}).write();
-
-    if (message.content === prefix + "xp") {
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Stat des XP de ${message.author.username}`)
-            .setColor("#58FAF4")
-            .setDescription("Affichge des XP")
-            .addField("XP :", `${xpfinal[1]}`)
-            .setFooter("Enjoy :p")
-        message.channel.send({embed: xp_embed});  
-}}});
